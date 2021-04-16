28480d52ddcc5e32ca4c28ffc385d8cbecf1355f
Justin Griffin <griffin.justintime@gmail.com>
2021-04-16 09:45:25 -0400

    working on delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,16 @@
+const { fstat } = require("node:fs");
+
 module.exports = app => {
 
 
     app.get("/api/notes", function(req, res) {
         res.json(notes);
     });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        fs.readFile("db/db.json", "utf8", (err, data) => {
+            const noteArr = JSON.parse(data);
+            const idNum = req.params.id;
+            
+        })
+    })
