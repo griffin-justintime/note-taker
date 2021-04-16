a8106003234d2f406329b4f5313df36d26b5a40b
Justin Griffin <griffin.justintime@gmail.com>
2021-04-16 13:47:14 -0400

    app.get work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,21 @@
-const { fstat } = require("node:fs");
+// const { fstat } = require("node:fs");
 
 module.exports = app => {
 
 
     app.get("/api/notes", function(req, res) {
-        res.json(notes);
+        fs.readFile('./db/db.json', "utf-8", (err, data)=> {
+            if (err) throw err
+        res.json(JSON.parse(data))
+    })
     });
 
+
+
     app.delete('/api/notes/:id', (req, res) => {
-        fs.readFile("db/db.json", "utf8", (err, data) => {
+        fs.readFile("db/db.json", "utf-8", (err, data) => {
             const noteArr = JSON.parse(data);
             const idNum = req.params.id;
-            
+
         })
-    })
+    });
